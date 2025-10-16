a25f9a61cb6974dfc91e03d3ad583e1c5da9654e
bruceathy <67832593+bruceathy@users.noreply.github.com>
2025-10-16 10:14:12 -0400

    away

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,11 +1,12 @@
 import Navbar from "../components/Navbar";
+import Crying from "../assets/crying.gif";
 
 export default function ErrorPage() {
   return (
     <div>
       <Navbar />
       <main className="error-msg">
-        <img src="../imgs/crying.gif" className="gif" />
+        <img src={Crying} className="gif" />
         <h1>ERROR</h1>
         <p>Page not found.</p>
       </main>
