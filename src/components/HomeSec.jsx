d82fe4bf782b9776a20cc677a99995cbcd070b32
bruceathy <67832593+bruceathy@users.noreply.github.com>
2025-10-26 21:44:18 -0400

    up

--- src/components/HomeSec.jsx
+++ src/components/HomeSec.jsx
@@ -1,10 +1,9 @@
-import { Link } from "react-router-dom";
 import MenuOpt from "./MenuOpt";
 
 export default function HomeSec() {
   return (
     <main className="home-page">
-      <h1>WELCOME</h1>
+      <h1>WELCOME TO PLASMA USA</h1>
       <section className="menu-list">
         <MenuOpt text="Vitals" location={"/Vitals"} />
         <MenuOpt text="Search Donors" location={"/Donors"} />
