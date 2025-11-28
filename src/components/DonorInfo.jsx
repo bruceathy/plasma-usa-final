f26fdd46b9356d32d80319955adacce23b88aa5d
bruceathy <67832593+bruceathy@users.noreply.github.com>
2025-11-27 23:47:02 -0500

    up

--- src/components/DonorInfo.jsx
+++ src/components/DonorInfo.jsx
@@ -30,7 +30,7 @@ const fetchUser = async () => {
     const response = await fetch("https://randomuser.me/api");
     const data = await response.json();
     const user = data.results[0];
-    console.log(user);
+    console.log(user.name.first);
     return user;
   } catch (error) {
     console.error(error);
